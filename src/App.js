d49b2a5948faa3b33115daba75b63afe65fddaad
kaisanjay <sanjaykai.1994@gmail.com>
2018-05-17 13:11:14 +0530

    form component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserForm from './components/UserForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title"> HTTP call in React </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <UserForm />
+        
       </div>
     );
   }
---
+++ src/components/UserForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserForm = () => {
+	return (
+		<form>
+		  <input type="text" name="username" />
+		  <button>Submit</button>
+		</form>
+		);
+}
+
+
+
+export default UserForm;
